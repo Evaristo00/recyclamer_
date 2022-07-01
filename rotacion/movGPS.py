1031697c6c70d870d53d6f3f242114627d65517f
Evaristo00 <evarsitogaston@gmail.com>
2022-07-01 11:44:19 -0300

    modificaion en el movGPS

--- rotacion/movGPS.py
+++ rotacion/movGPS.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-# from tf.transformations import euler_from_quaternion
+from tf.transformations import euler_from_quaternion
 import roslib; #roslib.load_manifest('rotacion')
 import rospy
 import math 
 
-#tf2_msgs/TFMessage
-# import tf2_ros
-# import tf2_msgs.msg
-# from tf2_msgs.msg import TFMessage
+
+import utm
 import geometry_msgs.msg
 import sensor_msgs.msg
 from sensor_msgs.msg import NavSatFix
@@ -36,48 +34,67 @@ class publisher():
 
 
 p = publisher()
+angle = 0
+distance = 0
+ImuOrientation= 0
 
+def calculateAngle(x,y):
+    global ImuOrientation
+    #problema el angulo va 0-360-(-360)-(-0)
+    print("Imu angle: {a}".format(a=ImuOrientation))
+    return 0
 
 def calc_dif(data):
-    #print(data)
     print("GPS latitude: {lat}, longitud: {long}".format(lat = data.latitude,long= data.longitude))
-    global goalLat
-    global goalLong
-    print("Goal latitude: {lat}, longitud: {long}".format(lat = goalLat,long= goalLong))
-    diflat = data.latitude - goalLat
-    difLong = data.longitude - goalLong
-    print("Dif latitude: {lat}, longitud: {long}".format(lat = diflat,long= difLong))
-    diflat = int(diflat * 10**7)
-    diflat = float(diflat* 10**-2)
-    difLong = int(difLong * 10**7)
-    difLong = float(diflat * 10**-2)
-    print("Dif INT latitude: {lat}, longitud: {long}".format(lat = diflat,long= difLong))
-    distance = (diflat**2 + difLong**2)**0.5
-    angle = math.atan(difLong/diflat)
-    print("distance: {d}, angle: {a}".format(d = distance,a= angle))
-    p.sendLinealMessege(distance)
-    p.sendAngularMessege(angle)
+    posX , posY = utm.from_latlon(data.latitude, data.longitude)[0:2]
+    print("posX: {posX}, posY: {posY}".format(posX = posX,posY= posY))
+    global goalX
+    print("goalX :  {goalX}".format(goalX=goalX))
+    RelativeGoalX = goalX - posX
+    print("relative goalX: {g}".format(g=RelativeGoalX))
+    global goalY
+    print("goalY :  {goalY}".format(goalY=goalY))
+    RelativeGoalY = goalY - posY
+    print("relative goalY: {g}".format(g=RelativeGoalY))
+
+    distance = (RelativeGoalX**2 + RelativeGoalY**2)**0.5
+    print("distance {d}".format(d=distance) )
+    angle = calculateAngle(RelativeGoalX,RelativeGoalY)
+    
+    print("\n\n ------------------------------- \n\n")
+
+
 
     
+def setImu(data):
     
-
+    w_orientation = data.orientation.w
+    x_orientation = data.orientation.x
+    y_orientation = data.orientation.y
+    z_orientation = data.orientation.z
+    global ImuOrientation
+    ImuOrientation = euler_from_quaternion(quaternion=[x_orientation,y_orientation,z_orientation,w_orientation])[2]
+    # print("caso ywzw:{a} ".format(a=ImuOrientation))
+    ImuOrientation = ImuOrientation * 360 / 3.14
+    # print("angulo corrimiento:{a} ".format(a=ImuOrientation))
 
 
 
 def listener():
+    rospy.Subscriber("/wamv/sensors/imu/imu/data", sensor_msgs.msg.Imu, setImu)
     rospy.Subscriber("/wamv/sensors/gps/gps/fix", sensor_msgs.msg.NavSatFix, calc_dif)
     print("se subscribe posicion actual")
     rospy.spin()
 
 
 
-
 if __name__ == '__main__':
     rospy.init_node('movGPS')
-    global goalLat
-    # goalLat = float(input('Enter a Latitude: '))
-    goalLat = 21.31110
-    global goalLong
-    # goalLong= float(input('Enter a Longitude: '))
-    goalLong = -157.88854
+    global goalX
+    # goalX = float(input('Enter a Latitude: '))
+    goalX = 21.31109
+    global goalY
+    # goalY= float(input('Enter a Longitude: '))
+    goalY = -157.88858
+    goalX, goalY = utm.from_latlon(goalX,goalY)[0:2]
     listener()
